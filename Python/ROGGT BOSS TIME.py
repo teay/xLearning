632662a1bff8665bb92f43752c3714ee604f7d00
teay <teay20000@gmail.com>
2023-04-23 02:02:45 +0700

    Update ROGGT BOSS TIME.py

--- Python/ROGGT BOSS TIME.py
+++ Python/ROGGT BOSS TIME.py
@@ -1,29 +1,11 @@
 # pip install python-dateutil มีแล้วไม่ต้องทำ
 from datetime import datetime, date
 from dateutil.relativedelta import relativedelta, SA, TH
+import pandas as pd
 
-def demo1():
-    now = datetime.today()
-    today = date.today()
-    next_3_m = today + relativedelta(months=3)
-    print('จากวันนี้           ',today)
-    print('บวกไป 3เดือน จะเป็น',next_3_m)
+# ดึงเวลาบอสตาย ได้เวลาบอสเกิด
 
-def demo2():
-    now = datetime.today()
-    print(now)
-    t1 = now + relativedelta(hours=3, minutes=30)
-    print(t1)
-
-demo1()
-demo2()
-
-nameBOSS = []
-pictureBOSS = []
-timeDelayAfterdie = []
-mapBOSS = []
-positionOnMapBossDie= []
-dateBossDie = []
-timeBossDie = []
-DelayTimeIsMinute = []
-timeBossHappen = []
+now = datetime.today()
+print("เวลาบอสตาย",now)
+DateTimeBaphometHappen = now + relativedelta(hours=2, minutes=0)
+print("เวลาบอสเกิด"        ,DateTimeBaphometHappen)
