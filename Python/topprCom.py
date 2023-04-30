c2d9c6e6d0b1eff0136465de7b6cdcd218d406c1
teay <teay20000@gmail.com>
2023-05-01 00:37:21 +0700

    updateชื่อ
    youtube.com@maoloop01
    youtube.com@prasertcbs
    youtube.com@ClassStartAcademy

---
+++ Python/topprCom.py
@@ -0,0 +1,12 @@
+
+# greeting someone using python
+#defining a function named greet
+
+def greet(name):
+  print("Hello "+name+" ,greetings from toppr!")
+name="Sanjana"
+
+#uncomment the next line to take input from user
+#name=input("Enter your name : ")
+#Calling the greet function and passing name as parameter
+greet(name)
