e9aa36f4da6fecc058eeea0c0332ec495b89d8ac
teay <teay20000@gmail.com>
2023-05-01 03:23:29 +0700

    การใช้ While Loop แบบที่ 1Pre-test loop ในภาษา Python ลองทำโจทย์ แบบที่ 1Pre-test loop
    การใช้ While Loop แบบที่ 1Pre-test loop ในภาษา Python ลองทำโจทย์ แบบที่ 1Pre-test loop

---
+++ Python/youtube.com@ClassStartAcademy/การใช้ While Loop แบบที่ 1Pre-test loop ในภาษา Python ลองทำโจทย์ แบบที่ 1Pre-test loop.py
@@ -0,0 +1,8 @@
+total = 0 # total
+counter = 1 # counter
+n = int(input('How many numbers?'))# n
+while counter <= n: # while
+    x = float(input('Enter anumber:'))# x
+    total += x # total += x
+    counter += 1 # counter += 1
+print('Total =', total)# print total
