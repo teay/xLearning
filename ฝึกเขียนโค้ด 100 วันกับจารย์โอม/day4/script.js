48d0379cac19a3e73f8985b74ca17fcc5bf8dbf2
teay <teay20000@gmail.com>
2023-05-12 23:55:22 +0700

    tabs
    ยังไม่เสร็จ

--- ฝึกเขียนโค้ด 100 วันกับจารย์โอม/day4/script.js
+++ ฝึกเขียนโค้ด 100 วันกับจารย์โอม/day4/script.js
@@ -1,7 +1,7 @@
 function openCity(evt, cityName) {
 
-    let tablinks = document.getElementsByClassName("tablinks")
-    let tabcontent = document.getElementsByClassName("tabcontent")
+    let tablinks = document.getElementsByClassName("tablinks");
+    let tabcontent = document.getElementsByClassName("tabcontent");
     let i;
 
     for (i = 0; i < tabcontent.length; i++) {
@@ -9,10 +9,11 @@ function openCity(evt, cityName) {
     }
 
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i]className.replace(" active", "")
+        tablinks[i].className = tablinks[i].className.replace(" active", "")
     }
 
     document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += "active";
+   evt.currentTarget.className += " active"; 
+
 
 }
