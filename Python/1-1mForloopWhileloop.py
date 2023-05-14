37f478696fd3b15e733381a97b5eae42f643a10d
teay <teay20000@gmail.com>
2023-05-14 23:54:55 +0700

    ForloopWhileloop

---
+++ Python/1-1mForloopWhileloop.py
@@ -0,0 +1,10 @@
+print("ใช้ for loop")
+for i in range(1, 11):
+    print(i)
+print("####################")
+print("ใช้ while loop")
+i = 1
+while i <= 1000000:
+    print(i)
+    i += 1
+print("####################")
